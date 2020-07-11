cd888b6445ea019c00da6fcb9b27b2928ab9bb46
ItsukiIshizuka <56192097+ItsukiIshizuka@users.noreply.github.com>
2020-07-11 18:53:15 +0900

    first commit

---
+++ app/javascript/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id = "app">{{message}}</div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      // この文字列が画面に表示されている
+      message: "test"
+    }
+  }
+}
+</script>
+
+<style>
+</style>
